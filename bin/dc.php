7d34d97a774daa0d9c3c560601b6f1b3249df1d4
Hendrik Heneke <hendrik@pixelwunder.net>
2016-11-21 07:39:02 +0100

    Locate autoload.php when installed as a dependency.

--- bin/dc.php
+++ bin/dc.php
@@ -1,4 +1,15 @@
 <?php
-require_once(__DIR__ . '/../vendor/autoload.php');
+
+$found = false;
+foreach ([__DIR__ . '/../../../autoload.php', __DIR__ . '/../vendor/autoload.php'] as $file) {
+    if (file_exists($file)) {
+        require_once($file);
+        $found = true;
+        break;
+    }
+}
+if (!$found) {
+    throw new RuntimeException('Could not locate autoload.php!');
+}
 
 \HHIT\Doctrine\Console\DoctrineClient::create()->run();
